df3334a2cdf204ed7be33d13cdd54a6790ac0dcd
Johan Fogelström <zapmew@gmail.com>
2014-11-02 23:24:33 +0100

    Do not forget the ast definition

---
+++ aspartasm/ast.go
@@ -0,0 +1,18 @@
+package aspartasm
+
+type ast struct {
+	token
+	children []ast
+}
+
+func newAst() *ast {
+	return &ast{token{unknown, "root"}}
+}
+
+//Appends the token to the tree as a child node.
+//Returns the new child node.
+func (a *ast) append(t token) *ast {
+	child := ast{t, nil}
+	a.children = append(a.children, child)
+	return &child
+}
