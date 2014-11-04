753105741d0709ab2e491c7f9c73945f668cb869
Johan Fogelström <zapmew@gmail.com>
2014-11-04 14:28:36 +0100

    Updated main to allow new partial assembly features

--- aspartasm/cola/main.go
+++ aspartasm/cola/main.go
@@ -1,14 +1,18 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
-	"github.com/zephyyrr/soda/aspartasme"
+	"github.com/zephyyrr/soda/aspartasm"
 	"log"
 	"os"
 )
 
 var (
-	output = flag.String("o", "a.sc", "Sets output name of assembled binary")
+	verbose   = flag.Bool("v", false, "Verbose output")
+	output    = flag.String("o", "a.sc", "Sets output name of assembled binary")
+	linearize = flag.Bool("l", false, "Only linearize (input is pre-parsed AST)")
+	parse     = flag.Bool("p", false, "Stop after parsing. Output is AST.")
 )
 
 func main() {
@@ -28,5 +32,49 @@ func main() {
 		log.Fatalf("Error opening output file %s for writing: %s", *output, err)
 	}
 
-	aspartasm.Assemble(in, out)
+	if *verbose {
+		log.Println("Input:", flag.Arg(0))
+		log.Println("Output:", *output)
+	}
+
+	if *parse {
+		if *verbose {
+			log.Println("Mode:", "Parse-only")
+		}
+		tree, err := aspartasm.Parse(in)
+		if err != nil {
+			log.Fatalln(err)
+		}
+		enc := json.NewEncoder(out)
+
+		if err := enc.Encode(tree); err != nil {
+			log.Fatalln(err)
+		}
+
+	} else if *linearize {
+		if *verbose {
+			log.Println("Mode:", "Linearize-only")
+		}
+		dec := json.NewDecoder(in)
+		var tree aspartasm.AST
+		if err := dec.Decode(&tree); err != nil {
+			log.Fatalln(err)
+		}
+
+		if *verbose {
+			log.Println(tree)
+		}
+		err := aspartasm.AssembleAst(tree, out)
+		if err != nil {
+			log.Println(err)
+		}
+	} else {
+		if *verbose {
+			log.Println("Mode:", "Assemble")
+		}
+		err := aspartasm.Assemble(in, out)
+		if err != nil {
+			log.Println(err)
+		}
+	}
 }
