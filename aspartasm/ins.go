1775cc2e31c1a993e134f0b903fa22070f0f5520
Oskar Segersvärd <victorystick@gmail.com>
2014-11-07 17:30:19 +0100

    Wrote more tests

--- aspartasm/ins.go
+++ aspartasm/ins.go
@@ -1,24 +1,18 @@
 package aspartasm
 
 import (
+	"bufio"
 	"bytes"
+	"encoding/binary"
+	"errors"
 	"fmt"
+	"github.com/zephyyrr/soda"
+	"io"
 )
 
-var opToString = make(map[Op]string)
-
-func init() {
-	for k, v := range ops {
-		opToString[Op(v)] = k
-	}
-}
-
-// A Soda opcode
-type Op byte
-
-func (op Op) String() string {
-	return opToString[op]
-}
+var (
+	ErrNoMagicBytes = errors.New("No magic bytes")
+)
 
 // A Soda operation argument
 type Arg fmt.Stringer
@@ -45,14 +39,57 @@ func (i Inst) String() string {
 	return buf.String()
 }
 
+// A register
 type Reg byte
 
 func (r Reg) String() string {
 	return fmt.Sprintf("r%d", r)
 }
 
+// An immediate value
 type Imm uint32
 
 func (i Imm) String() string {
 	return fmt.Sprintf("%d", i)
+}
+
+// ReadInstructions reads a sequence of Soda instructions from an io.Reader.
+// The first 4 bytes read must be the Soda magic bytes.
+func ReadInstructions(raw io.Reader) ([]Inst, error) {
+	in := bufio.NewReader(raw)
+
+	var magic uint32
+
+	if err := binary.Read(in, binary.BigEndian, &magic); err != nil {
+		return nil, err
+	}
+
+	// Ensure magic bytes
+	if magic != uint32(soda.MagicBytes) {
+		return nil, ErrNoMagicBytes
+	}
+
+	instrs := make([]Inst)
+
+	// Loop while not EOF
+	for {
+		opCode, err := in.ReadByte()
+
+		switch err != nil; err {
+		case EOF:
+			return instrs, nil
+		default:
+			return nil, err
+		}
+
+		rdr := getArgumentReader(opCode)
+
+		args, err := rdr(in)
+
+		if err != nil {
+			return nil, err
+		}
+
+		instrs = append(instrs, Inst{Op(opCode), args})
+	}
 }
--- aspartasm/ins_test.go
+++ aspartasm/ins_test.go
@@ -5,7 +5,11 @@ import (
 )
 
 var instTable = map[string]Inst{
-	"HALT": Inst{Op(0), nil},
+	"HALT":          Inst{HALT, nil},
+	"ADD r1 r1 r2":  Inst{ADD, []Arg{Reg(1), Reg(1), Reg(2)}},
+	"MALC r231 r17": Inst{MALC, []Arg{Reg(231), Reg(17)}},
+	"PRNC 10":       Inst{PRNC, []Arg{Imm(10)}},
+	"JMP r32":       Inst{JMP, []Arg{Reg(32)}},
 }
 
 func TestInstString(t *testing.T) {
