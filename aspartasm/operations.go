e4bb483b4684335dc9eb3520ecbcbfa4479b0b90
Oskar Segersvärd <victorystick@gmail.com>
2014-11-07 17:59:04 +0100

    Added argument readers, and table from operation to reader

--- aspartasm/operations.go
+++ aspartasm/operations.go
@@ -1,6 +1,8 @@
 package aspartasm
 
 import (
+	"bufio"
+	"encoding/binary"
 	"fmt"
 )
 
@@ -146,6 +148,58 @@ const (
 	PRNC
 )
 
+var opToArgReader = map[Op]argReader{
+	HALT: noRegs,
+
+	NRS:  noRegs,
+	PRS:  noRegs,
+	MOVS: threeRegs,
+
+	ZERO: oneReg,
+	ADD:  threeRegs,
+	SUB:  threeRegs,
+	MUL:  threeRegs,
+	DIV:  threeRegs,
+	MOD:  threeRegs,
+	POW:  threeRegs,
+
+	ADDU: threeRegs,
+	SUBU: threeRegs,
+	MULU: threeRegs,
+	DIVU: threeRegs,
+	MODU: threeRegs,
+	POWU: threeRegs,
+
+	AND: threeRegs,
+	OR:  threeRegs,
+	XOR: threeRegs,
+	INV: threeRegs,
+	LSH: threeRegs,
+	RSH: threeRegs,
+
+	MALC: twoRegs,
+	LDW:  threeRegs,
+	LDB:  threeRegs,
+	LDI:  oneRegImm,
+	STW:  threeRegs,
+	STB:  threeRegs,
+	FREE: oneReg,
+
+	JMP:   oneReg,
+	JMPE:  threeRegs,
+	JMPN:  threeRegs,
+	JMPL:  threeRegs,
+	JMPLE: threeRegs,
+	BRA:   oneReg,
+	BRAE:  threeRegs,
+	BRAN:  threeRegs,
+	BRAL:  threeRegs,
+	BRALE: threeRegs,
+
+	PRNI: oneReg,
+	PRNC: oneReg,
+}
+
 var opToString = make(map[Op]string)
 
 func init() {
@@ -163,4 +217,99 @@ func (op Op) String() string {
 	}
 
 	return fmt.Sprintf("Op(%x)", byte(op))
+}
+
+func (op Op) ArgReader() (argReader, error) {
+	reader, ok := opToArgReader[op]
+
+	if !ok {
+		return nil, ErrInvalidOperation
+	}
+
+	return reader, nil
+}
+
+type argReader func(*bufio.Reader) ([]Arg, error)
+
+func noRegs(in *bufio.Reader) ([]Arg, error) {
+	for i := 0; i < 3; i++ {
+		if _, err := in.ReadByte(); err != nil {
+			return nil, err
+		}
+	}
+
+	return nil, nil
+}
+
+func oneReg(in *bufio.Reader) ([]Arg, error) {
+	r1, err := in.ReadByte()
+
+	if err != nil {
+		return nil, err
+	}
+
+	for i := 0; i < 2; i++ {
+		if _, err := in.ReadByte(); err != nil {
+			return nil, err
+		}
+	}
+
+	return []Arg{Reg(r1)}, nil
+}
+
+func twoRegs(in *bufio.Reader) ([]Arg, error) {
+	r1, err := in.ReadByte()
+
+	if err != nil {
+		return nil, err
+	}
+
+	r2, err := in.ReadByte()
+
+	if err != nil {
+		return nil, err
+	}
+
+	if _, err := in.ReadByte(); err != nil {
+		return nil, err
+	}
+
+	return []Arg{Reg(r1), Reg(r2)}, nil
+}
+
+func threeRegs(in *bufio.Reader) ([]Arg, error) {
+	r1, err := in.ReadByte()
+
+	if err != nil {
+		return nil, err
+	}
+
+	r2, err := in.ReadByte()
+
+	if err != nil {
+		return nil, err
+	}
+
+	r3, err := in.ReadByte()
+
+	if err != nil {
+		return nil, err
+	}
+
+	return []Arg{Reg(r1), Reg(r2), Reg(r3)}, nil
+}
+
+func oneRegImm(in *bufio.Reader) ([]Arg, error) {
+	r1, err := in.ReadByte()
+
+	if err != nil {
+		return nil, err
+	}
+
+	var imm uint32
+	if err := binary.Read(in, binary.BigEndian, &imm); err != nil {
+		return nil, err
+	}
+
+	return []Arg{Reg(r1), Imm(imm)}, nil
 }
